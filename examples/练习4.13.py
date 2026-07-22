a98470fd7d1960a75aee5bb7731e80674f130f0a
SnowBallAxe <Fei_Shuang@outlook.com>
2026-07-22 21:18:20 +0800

    add exercises 4.11and 4.13

---
+++ examples/练习4.13.py
@@ -0,0 +1,8 @@
+menu=("egg","bacon","spam","sausages","beans")
+for food in menu:
+    print(food)
+
+menu=("egg","tomato","spam","sausages","bread")
+print("\n")
+for food in menu:
+    print(food)
